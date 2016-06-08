282c82f188bb187cdb5f64bb8516fa10f6f014bc
Toure nathan <Toure nathan>
2016-06-08 15:01:41 +0100

    Build and displayed dummy weather data

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +62,30 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // We will now create some dummy data for the Listview
+
+            // Sample Weather information for a week: day - wether - high/low
+            String[] weeklyWeather = new String[]{
+                    "Today - Sunny - 88/63",
+                    "Tomorrow - Fuggy - 70/46",
+                    "Weds - Cloudy - 72/63",
+                    "Thurs - Rainy - 64/51",
+                    "Fri - Foggy - 70/46",
+                    "Sat - Sunny - 70/68",
+                    "Sun - Cloudy - 71/62"
+            };
+
+            ArrayList<String> forecastList = new ArrayList<>();
+            forecastList.addAll(Arrays.asList(weeklyWeather));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                    R.layout.list_item_forecast, // ID of list item layout
+                    R.id.list_item_forecast_textview, //ID of textview to populate
+                    forecastList);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
             return rootView;
         }
     }
